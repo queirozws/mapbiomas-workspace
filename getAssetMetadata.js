b172e5454cb0f1bb61cc6cf14f5e373b136a7e95
queirozws <queirozws@gmail.com>
2021-10-28 02:01:08 +0000

    Updated getAssetMetadata.js (added 23 lines, deleted 23 lines).

--- getAssetMetadata.js
+++ getAssetMetadata.js
@@ -5,29 +5,29 @@
  */
  
  var products = [
-    // {
-    //     asset_id: "projects/mapbiomas-workspace/COLECAO6/classificacao",
-    //     description: 'Dados de classificação da coleção 6 do Brasil',
-    //     collection: 6,
-    //     initiative: 'brazil',
-    //     theme: 'classification',
-    //     type: 'mosaic-singleband', // | "classification-singleband" | "mosaic-multiband",
-    //     countries: ['brazil'],
-    //     source: ["source1", "source2"],
-    //     metadata: {
-    //         bands: {
-    //             prefix: 'classification',
-    //         },
-    //         years: [
-    //             "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993",
-    //             "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002",
-    //             "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
-    //             "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"
-    //           ],
-    //         biome: ["biome"],
-    //         version: '1',
-    //     }
-    // },
+    {
+        asset_id: "projects/mapbiomas-workspace/COLECAO6/classificacao",
+        description: 'Dados de classificação da coleção 6 do Brasil',
+        collection: 6,
+        initiative: 'brazil',
+        theme: 'classification',
+        type: 'mosaic-singleband', // | "classification-singleband" | "mosaic-multiband",
+        countries: ['brazil'],
+        source: ["source1", "source2"],
+        metadata: {
+            bands: {
+                prefix: 'classification',
+            },
+            years: [
+                "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993",
+                "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002",
+                "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
+                "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"
+              ],
+            biome: ["biome"],
+            version: '1',
+        }
+    },
     {
         initiative: 'brazil',
         collection: 6,
