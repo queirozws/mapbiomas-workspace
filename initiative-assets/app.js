b42bca8d68de3db74cadd7e10fb114b8e50cc950
queirozws <queirozws@gmail.com>
2021-11-03 16:31:40 -0300

    create app.js

---
+++ initiative-assets/app.js
@@ -0,0 +1,22 @@
+App = {
+
+    "options": {
+        version: "0.0.1",
+        assets: {},
+
+    },
+    "init": function() {},
+    "setVersion": function() {},
+    "getPrevalenceList": function() {},
+    "getClassifications": function() {},
+    "getMosaics": function() {},
+    "getRegions": function() {},
+    "getCollections": function() {},
+    "calculateStates": function() {},
+    "calculateIncidents": function() {},
+    "calculateFilterDiff": function() {},
+    "loadLayers": function() {},
+    "loadData": function() {},
+    "ui": ,
+  
+  }
