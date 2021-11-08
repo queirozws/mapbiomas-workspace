677931840328084bf358576895131cc6f3b66e98
queirozws <queirozws@gmail.com>
2021-11-08 22:33:03 +0000

    Updated initiative-assets/view.js (added 130 lines, deleted 75 lines).

--- initiative-assets/view.js
+++ initiative-assets/view.js
@@ -11,7 +11,7 @@ var App = {
             type: "panel",
             name: "header",
             data: null,
-            parent: this.,
+            parent: "mainPanel",
             style: {
               height: "350px",
               width: "350px",
@@ -22,22 +22,16 @@ var App = {
             type: "select",
             name: "Choose an option",
             data: null,
-            parent: this,
-            style: {
-              // height: "35",
-              width: "100px",
-            },
+            parent: "mainPanel",
+            style: {},
             child: "none",
           },
           {
             type: "button",
             name: "Visualize data",
             data: null,
-            parent: this.,
-            style: {
-              // height: "35px",
-              width: "100px",
-            },
+            parent: "mainPanel",
+            style: {},
             child: "none",
           }          
         ]
@@ -47,82 +41,143 @@ var App = {
   }
 }
 
-function render(obj) {
-  
-  var type = {
-    "panel": {
-        constructor: function (obj) {
-          
-            var params = {
-                widgets: null,
-                layout: null,
-                style: obj.style
-            }
-            
-            var panel = ui.Panel( params );
-            
-            ui.root.add(panel);
-            
-        },
-    "select": {
-        constructor: function (obj) {
-          
-          var params = {
-              items: null,
-              placeholder: null,
-              value: null,
-              onChange: null,
-              disabled: null,
-              style: obj.style
-          }
-          
-          ui.Select(params)
-          
-        },
-    "button": {
-        constructor: function (obj) {
-          
-          var params = obj
-          
-          ui.Button({
-              label: null,
-              onClick: null,
-              disabled: null,
-              style: obj.style
-          })
-
-    },
-  }
-  
+var obj = {
+  a: "nome",
+  b: this
 }
 
-ee.Select
+var params = {
+    widgets: null,
+    layout: null,
+    style: {
+        height: "350px",
+        width: "350px",
+    }
+}
+
+var panel = ui.Panel( params );
+
+var header = ui.Panel({ style: {width: "100px"} });
+
+// ui.root.add(panel);
+
+var params = {
+    items: null,
+    placeholder: null,
+    value: null,
+    onChange: null,
+    disabled: null,
+    style: obj.style
+}
+
+var select = ui.Select();
+
+var select2 = ui.Select();
+
+// panel.add(header);
+
+// panel.add(select);
+
+// panel.add(select2);
+
+
+
+// print(obj);
+print(App.view.ui[0].child[0].parent)
+
+function render(obj) {
+  
+    var type = {
+        "panel": {
+            constructor: function (obj) {
+              
+                var params = {
+                    widgets: null,
+                    layout: null,
+                    style: obj.style
+                }
+                
+                var panel = ui.Panel( params );
+                
+                ui.root.add(panel);
+                
+            },
+        },
+        "select": {
+            constructor: function (obj) {
+              
+                var params = {
+                    items: ["1","2","3","4","5"],
+                    placeholder: "Choose an option",
+                    value: null,
+                    onChange: null,
+                    disabled: null,
+                    style: obj.style
+                }
+                
+                var select = ui.Select(params);
+                
+                // panel.add(select);
+                
+                print(select);
+
+            }
+              
+        },
+        "button": {
+            constructor: function (obj) {
+              
+                var params = {
+                    label: obj.name,
+                    onClick: null,
+                    disabled: null,
+                    style: obj.style
+                };
+                
+                var button = ui.Button(params);
+                
+                print(button);
+
+            }
+        },
+
+    };
+    
+    type[obj.type].constructor(obj);
+  
+}
 
 function walk(obj, callback) {
   
   var nodeList = obj.view.ui;
   
-  print(nodeList)
+  print(nodeList);
   
   var keyList = Object.keys(obj);
   
   keyList.forEach(
-    function (key) {
-      
-      obj[key]
-      
-      console.log(key+" key")
-    }
+      function (key) {
+        
+        // obj[key];
+        
+        console.log(key+" key")
+        
+      }
   )
   
-  print(keyList)
+  print(keyList);
 
   
   // callback()
 }
 
 
-render(App);
+render(App.view.ui[0].child[0]);
+render(App.view.ui[0].child[1]);
+render(App.view.ui[0].child[2]);
+
+print(ui.root.widgets())
+// print(panel.widgets())
 
 
 
