8fb1e7243483fd47ecfc74d5af1f51a70331970a
queirozws <queirozws@gmail.com>
2021-11-11 02:30:26 +0000

    Updated initiative-assets/view2.js (added 144 lines, deleted 140 lines).

--- initiative-assets/view2.js
+++ initiative-assets/view2.js
@@ -8,7 +8,133 @@ var logos = require('users/mapbiomas/modules:Logos.js');
 
 var logo = logos.mapbiomas;
 
+var painels = {};
+
+var index = 0;
+
 var App = {
+    data: {},
+    functions: {
+        render: function (obj, index) {
+    
+            // print(obj.id);
+            
+            var type = {
+                "panel": {
+                    constructor: function (obj) {
+                        
+                        // var layout = ui.Panel.Layout.absolute();
+                        var layout = ui.Panel.Layout.flow("vertical", false);
+                        
+                        var params = {
+                            widgets: obj.params.widgets || null, // || default params
+                            layout: obj.params.layout || layout, // || default params
+                            style: obj.params.style || {width: "700px"} // || default params
+                        }
+                        
+                        var panel = ui.Panel( params );
+                        
+                        if (obj.parentId === "root") {
+                            
+                            print(panel);
+                            // Map.add(panel);
+                            // ui.root.add(panel);
+                            
+                            painels[obj.id] = panel;
+                            
+                        } else {
+                          
+                            painels[obj.parentId].insert(index, panel);
+        
+                            painels[obj.id] = panel;
+        
+                        }
+                        
+                    },
+                },
+                "select": {
+                    constructor: function (obj) {
+                      
+                        obj.parentId
+                      
+                        var params = {
+                            items: obj.params.items || ["1","2","3","4","5"],
+                            placeholder: obj.params.placeholder || "Choose an option",
+                            value: obj.params.value || null,
+                            onChange: obj.params.onChange || null,
+                            disabled: obj.params.disabled || null,
+                            style: obj.params.style || null
+                        }
+                        
+                        var select = ui.Select(params);
+                        
+                        painels[obj.parentId].insert(index, select);
+                        
+                    }
+                      
+                },
+                "button": {
+                    constructor: function (obj) {
+                      
+                        var params = {
+                            label: obj.params.label || "button (" + obj.parentId + " panel)", // || default value
+                            onClick: obj.params.onClick || function () {print("button (" + obj.parentId + " panel)")}, // || default value
+                            disabled: obj.params.disabled || false, // || default value
+                            style: obj.params.style || {} // || default value
+                        };
+                        
+                        var button = ui.Button(params);
+                        
+                        painels[obj.parentId].insert(index, button);
+                        
+                    }
+                },
+                "label": {
+                    constructor: function (obj) {
+                      
+                        var params = {
+                            value: obj.params.label || "some label (" + obj.parentId + " panel)", // || default value
+                            targetUrl: obj.params.targetUrl || "", // || default value
+                            style: obj.params.style || {} // || default value
+                        };
+                        
+                        var button = ui.Label( params );
+                        
+                        painels[obj.parentId].insert(index, button);
+                        
+                    }
+                },
+                "map": {
+                    constructor: function (obj) {
+                      
+                        var params = {
+                            center: obj.params.center || {lat: -6, lon: -54, zoom: 3}, // || default value
+                            onClick: obj.params.onClick || null, // || default value
+                            style: obj.params.style || {margin: "15px"} // || default value
+                        };
+                        
+                        var map = ui.Map( params );
+                        
+                        painels[obj.parentId].insert(3, map);
+                        
+                    }
+                },
+        
+            };
+            
+            if (painels[obj.parentId] === "undefined" ) {
+            
+                print("O painel selecionado para o widget " + obj.id + " não existe!")
+        
+            } else {
+                
+                type[obj.type].constructor(obj);
+                
+            }
+          
+        },
+        other: function() {}
+    },
     view: {
         ui: [
             {
@@ -175,149 +301,27 @@ var App = {
             },
         ],
   
+    },
+    init: function() {
+        
+        var render = this.functions.render;
+        
+        this.view.ui.forEach(
+            function (obj) {
+                
+                index = index + 1;
+                
+                render(obj, index);
+
+            }
+        );
+        
+
     }
 
 }
 
-var painels = {};
-
-function render(obj, index) {
-    
-    // print(obj.id);
-    
-    var type = {
-        "panel": {
-            constructor: function (obj) {
-                
-                // var layout = ui.Panel.Layout.absolute();
-                var layout = ui.Panel.Layout.flow("vertical", false);
-                
-                var params = {
-                    widgets: obj.params.widgets || null, // || default params
-                    layout: obj.params.layout || layout, // || default params
-                    style: obj.params.style || {width: "700px"} // || default params
-                }
-                
-                var panel = ui.Panel( params );
-                
-                if (obj.parentId === "root") {
-                    
-                    print(panel);
-                    // Map.add(panel);
-                    // ui.root.add(panel);
-                    
-                    painels[obj.id] = panel;
-                    
-                } else {
-                  
-                    painels[obj.parentId].insert(index, panel);
-
-                    painels[obj.id] = panel;
-
-                }
-                
-            },
-        },
-        "select": {
-            constructor: function (obj) {
-              
-                obj.parentId
-              
-                var params = {
-                    items: obj.params.items || ["1","2","3","4","5"],
-                    placeholder: obj.params.placeholder || "Choose an option",
-                    value: obj.params.value || null,
-                    onChange: obj.params.onChange || null,
-                    disabled: obj.params.disabled || null,
-                    style: obj.params.style || null
-                }
-                
-                var select = ui.Select(params);
-                
-                painels[obj.parentId].insert(index, select);
-                
-            }
-              
-        },
-        "button": {
-            constructor: function (obj) {
-              
-                var params = {
-                    label: obj.params.label || "button (" + obj.parentId + " panel)", // || default value
-                    onClick: obj.params.onClick || function () {print("button (" + obj.parentId + " panel)")}, // || default value
-                    disabled: obj.params.disabled || false, // || default value
-                    style: obj.params.style || {} // || default value
-                };
-                
-                var button = ui.Button(params);
-                
-                painels[obj.parentId].insert(index, button);
-                
-            }
-        },
-        "label": {
-            constructor: function (obj) {
-              
-                var params = {
-                    value: obj.params.label || "some label (" + obj.parentId + " panel)", // || default value
-                    targetUrl: obj.params.targetUrl || "", // || default value
-                    style: obj.params.style || {} // || default value
-                };
-                
-                var button = ui.Label( params );
-                
-                painels[obj.parentId].insert(index, button);
-                
-            }
-        },
-        "map": {
-            constructor: function (obj) {
-              
-                var params = {
-                    center: obj.params.center || {lat: -6, lon: -54, zoom: 3}, // || default value
-                    onClick: obj.params.onClick || null, // || default value
-                    style: obj.params.style || {margin: "15px"} // || default value
-                };
-                
-                var map = ui.Map( params );
-                
-                painels[obj.parentId].insert(3, map);
-                
-            }
-        },
-
-    };
-    
-    if (painels[obj.parentId] === "undefined" ) {
-    
-        print("O painel selecionado para o widget " + obj.id + " não existe!")
-
-    } else {
-        
-        type[obj.type].constructor(obj);
-        
-    }
-  
-}
-
-function isArray(currentLevel) {
-    return currentLevel
-}
-
-// loop(App.view.ui, render);
-
-var index = 0;
-
-App.view.ui.forEach(
-    function (obj) {
-        
-        index = index + 1;
-        
-        render(obj, index)
-        
-        
-    }
-);
+App.init();
 
 // print(painels);
 
